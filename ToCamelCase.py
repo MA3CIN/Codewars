83b3399186702145acc60fb41037f3485102b1eb
Marcin Ziółkowski <marcin.zet@onet.pl>
2021-05-11 13:52:13 +0200

    Completed ToCamelCase!

---
+++ ToCamelCase.py
@@ -0,0 +1,12 @@
+import re
+def to_camel_case(text):
+    comp = re.split(', |_|-|\+', text)
+    return comp[0] + ''.join(x.title() for x in comp[1:])
+
+		
+test.describe("Testing function to_camel_case")
+test.it("Basic tests")
+test.assert_equals(to_camel_case(''), '', "An empty string was provided but not returned")
+test.assert_equals(to_camel_case("the_stealth_warrior"), "theStealthWarrior", "to_camel_case('the_stealth_warrior') did not return correct value")
+test.assert_equals(to_camel_case("The-Stealth-Warrior"), "TheStealthWarrior", "to_camel_case('The-Stealth-Warrior') did not return correct value")
+test.assert_equals(to_camel_case("A-B-C"), "ABC", "to_camel_case('A-B-C') did not return correct value")
